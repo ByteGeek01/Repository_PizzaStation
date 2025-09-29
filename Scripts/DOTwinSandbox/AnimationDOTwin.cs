dd7ac9ee1d28a6639e274e0b67465549957d3f22
ByteGeek01 <ssbluegoku30@gmail.com>
2025-09-29 13:01:54 -0300

    comentarios

--- Scripts/DOTwinSandbox/AnimationDOTwin.cs
+++ Scripts/DOTwinSandbox/AnimationDOTwin.cs
@@ -5,10 +5,13 @@ public class AnimationDOTwin : MonoBehaviour
 {
     public void Start()
     {
+        // Se mueve por 3 unidades, por 2 segundos
         transform.DOMoveX(transform.position.x + 3f, 2f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
 
+        // Rota en 360°
         transform.DORotate(new Vector3(0, 360, 0), 3f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.InOutSine);
 
+        // Escala el objeto por 1,5 segundos
         transform.DOScale(transform.localScale * 2f, 1.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
 }
